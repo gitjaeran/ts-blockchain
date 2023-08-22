f347724421e44d8985d6e17261ca322a9098a797
JaeranKIM <devjaeran@gmail.com>
2023-08-22 15:48:03 +0900

    @types/node, module, make block

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,27 @@
  * build 없이 타입스크립트 실행(빌드 없이 새로고침하고 싶을 때, 개발환경에서만 사용)
  * => ts-node가 컴파일할 필요없이 ts 코드를 대신 실행
  */
-console.log("hi!");
+
+//npm i @types/node : DefinitelyTyped
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
